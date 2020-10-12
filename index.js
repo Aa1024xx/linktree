1114cdb22885bad9f3b8185236fa338412e4dce3
Aa1024xx <48708642+Aa1024xx@users.noreply.github.com>
2020-10-12 17:48:22 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,115 @@
+const instagramSVG =
+  '<svg role="img" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg"><title>Instagram icon</title><path d="M12 0C8.74 0 8.333.015 7.053.072 5.775.132 4.905.333 4.14.63c-.789.306-1.459.717-2.126 1.384S.935 3.35.63 4.14C.333 4.905.131 5.775.072 7.053.012 8.333 0 8.74 0 12s.015 3.667.072 4.947c.06 1.277.261 2.148.558 2.913.306.788.717 1.459 1.384 2.126.667.666 1.336 1.079 2.126 1.384.766.296 1.636.499 2.913.558C8.333 23.988 8.74 24 12 24s3.667-.015 4.947-.072c1.277-.06 2.148-.262 2.913-.558.788-.306 1.459-.718 2.126-1.384.666-.667 1.079-1.335 1.384-2.126.296-.765.499-1.636.558-2.913.06-1.28.072-1.687.072-4.947s-.015-3.667-.072-4.947c-.06-1.277-.262-2.149-.558-2.913-.306-.789-.718-1.459-1.384-2.126C21.319 1.347 20.651.935 19.86.63c-.765-.297-1.636-.499-2.913-.558C15.667.012 15.26 0 12 0zm0 2.16c3.203 0 3.585.016 4.85.071 1.17.055 1.805.249 2.227.415.562.217.96.477 1.382.896.419.42.679.819.896 1.381.164.422.36 1.057.413 2.227.057 1.266.07 1.646.07 4.85s-.015 3.585-.074 4.85c-.061 1.17-.256 1.805-.421 2.227-.224.562-.479.96-.899 1.382-.419.419-.824.679-1.38.896-.42.164-1.065.36-2.235.413-1.274.057-1.649.07-4.859.07-3.211 0-3.586-.015-4.859-.074-1.171-.061-1.816-.256-2.236-.421-.569-.224-.96-.479-1.379-.899-.421-.419-.69-.824-.9-1.38-.165-.42-.359-1.065-.42-2.235-.045-1.26-.061-1.649-.061-4.844 0-3.196.016-3.586.061-4.861.061-1.17.255-1.814.42-2.234.21-.57.479-.96.9-1.381.419-.419.81-.689 1.379-.898.42-.166 1.051-.361 2.221-.421 1.275-.045 1.65-.06 4.859-.06l.045.03zm0 3.678c-3.405 0-6.162 2.76-6.162 6.162 0 3.405 2.76 6.162 6.162 6.162 3.405 0 6.162-2.76 6.162-6.162 0-3.405-2.76-6.162-6.162-6.162zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm7.846-10.405c0 .795-.646 1.44-1.44 1.44-.795 0-1.44-.646-1.44-1.44 0-.794.646-1.439 1.44-1.439.793-.001 1.44.645 1.44 1.439z"/></svg>'
+const facebookSVG =
+  '<svg role="img" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg"><title>Facebook icon</title><path d="M23.9981 11.9991C23.9981 5.37216 18.626 0 11.9991 0C5.37216 0 0 5.37216 0 11.9991C0 17.9882 4.38789 22.9522 10.1242 23.8524V15.4676H7.07758V11.9991H10.1242V9.35553C10.1242 6.34826 11.9156 4.68714 14.6564 4.68714C15.9692 4.68714 17.3424 4.92149 17.3424 4.92149V7.87439H15.8294C14.3388 7.87439 13.8739 8.79933 13.8739 9.74824V11.9991H17.2018L16.6698 15.4676H13.8739V23.8524C19.6103 22.9522 23.9981 17.9882 23.9981 11.9991Z"/></svg>'
+const githubSVG =
+  '<svg role="img" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg"><title>GitHub icon</title><path d="M19 0H5a5 5 0 00-5 5v14a5 5 0 005 5h14a5 5 0 005-5V5a5 5 0 00-5-5zm-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82A7.616 7.616 0 0012 7.868a7.643 7.643 0 00-2.003.269c-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118a3.092 3.092 0 00-.824 2.147c0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385A8 8 0 0112 4a8 8 0 012.534 15.59z"/></svg>'
+const twitterSVG =
+  '<svg role="img" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg"><title>GitHub icon</title><path d="M3,0 L21,0 C22.6568542,-3.04359188e-16 24,1.34314575 24,3 L24,21 C24,22.6568542 22.6568542,24 21,24 L3,24 C1.34314575,24 2.02906125e-16,22.6568542 0,21 L0,3 C-2.02906125e-16,1.34314575 1.34314575,3.04359188e-16 3,0 Z M9.40543979,19.2 C15.5354921,19.2 18.8873246,14.1204188 18.8873246,9.71811518 C18.8873246,9.57298429 18.8873246,9.4278534 18.8804136,9.28963351 C19.5300471,8.81968586 20.0967487,8.23225131 20.5459634,7.56188482 C19.9516178,7.82450262 19.3088953,8.00418848 18.6316178,8.08712042 C19.3227173,7.67246073 19.8479529,7.02282723 20.0967487,6.24188482 C19.4540262,6.62198953 18.7421937,6.89842932 17.9819843,7.0504712 C17.3738168,6.4008377 16.5099424,6 15.5493141,6 C13.7109895,6 12.2182147,7.49277487 12.2182147,9.33109948 C12.2182147,9.59371728 12.2458586,9.84942408 12.3080576,10.0913089 C9.53674869,9.95308901 7.08334555,8.62617801 5.4385288,6.60816754 C5.15517801,7.09884817 4.98931414,7.67246073 4.98931414,8.28062827 C4.98931414,9.4347644 5.57674869,10.4575916 6.47517801,11.0519372 C5.92920942,11.0381152 5.41779581,10.8860733 4.96858115,10.6372775 C4.96858115,10.6510995 4.96858115,10.6649215 4.96858115,10.6787435 C4.96858115,12.2959162 6.11580628,13.6366492 7.64313613,13.947644 C7.36669634,14.0236649 7.06952356,14.0651309 6.76543979,14.0651309 C6.55119895,14.0651309 6.34386911,14.0443979 6.13653927,14.0029319 C6.55810995,15.3298429 7.78826702,16.2904712 9.24648691,16.3181152 C8.10617277,17.2096335 6.66868586,17.7417801 5.10680105,17.7417801 C4.83727225,17.7417801 4.57465445,17.7279581 4.31203665,17.6934031 C5.77025654,18.6471204 7.52564921,19.2 9.40543979,19.2"/></svg>'
+
+
+const links = [
+  { name: 'Instagram', url: 'https://instagram.com/' },
+  { name: 'Facebook', url: 'https://www.facebook.com/' },
+  { name: 'GitHub', url: 'https://github.com/' },
+  { name: 'Twitter', url: 'https://twitter.com/' },
+]
+
+const staticURL = 'https://static-links-page.signalnerve.workers.dev'
+
+addEventListener('fetch', event => {
+  let url = event.request.url
+  let urlParse = url.substring(url.indexOf('.dev') + 4)
+  if (urlParse == '/links' || urlParse == '/links/')
+    event.respondWith(handleJSON())
+  else event.respondWith(handleRequest(event.request))
+})
+
+async function handleJSON() {
+  return new Response(JSON.stringify(links), {
+    headers: { 'content-type': 'application/json' },
+  })
+}
+
+async function handleRequest(request) {
+  const options = {
+    headers: {
+      'content-type': 'text/html',
+    },
+  }
+  let response = await fetch(staticURL, options)
+
+  const rewriter = new HTMLRewriter()
+    .on('div#links', new LinksTransformer(links))
+    .on('div#profile', new ProfileTransformer())
+    .on('img#avatar', new ImageTransformer())
+    .on('h1#name', new UsernameTransformer())
+    .on('div#social', new SocialTransformer())
+    .on('title', new TitleTransformer())
+    .on('body', new BackgroundTransformer())
+  return rewriter.transform(response)
+}
+
+class LinksTransformer {
+  constructor(links) {
+    this.links = links
+  }
+
+  async element(element) {
+    for (let i = 0; i < this.links.length; i++)
+      element.append(
+        '<a href="' + this.links[i].url + '">' + this.links[i].name + '</a>',
+        {
+          html: true,
+        },
+      )
+  }
+}
+
+class ProfileTransformer {
+  async element(element) {
+    element.removeAttribute('style')
+  }
+}
+
+class ImageTransformer {
+  async element(element) {
+    element.setAttribute('src', 'https://avatars0.githubusercontent.com/u/15454486?v=4')
+  }
+}
+
+class UsernameTransformer {
+  async element(element) {
+    element.setInnerContent('Hello, this XX')
+  }
+}
+
+class SocialTransformer {
+  async element(element) {
+    element.removeAttribute('style')
+    element.append('<a href="https://instagram.com">' + instagramSVG + '</a>', {
+      html: true,
+    })
+    element.append('<a href="https://facebook.com">' + facebookSVG + '</a>', {
+      html: true,
+    })
+    element.append('<a href="https://github.com/">' + githubSVG + '</a>', {
+      html: true,
+    })
+    element.append('<a href="https://twitter.com/">' + twitterSVG + '</a>', {
+      html: true,
+    })
+  }
+}
+
+class TitleTransformer {
+  async element(element) {
+    element.setInnerContent('WelcomeXX')
+  }
+}
+
+class BackgroundTransformer {
+  async element(element) {
+    element.setAttribute('class', 'bg-purple-400')
+  }
+}
